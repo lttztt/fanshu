b7e7f5e4abfc6777c08c722ae7adf430d6e96442
liuzhe <zhethink@gmail.com>
2018-09-03 14:56:31 +0800

    安装vue-progressbar，并且添加到项目，配置List路由测试

--- src/main.js
+++ src/main.js
@@ -6,6 +6,25 @@ import router from './router'
 import store from './store'
 import api from './api'
 
+// 加载进度条
+import VueProgressBar from 'vue-progressbar'
+const options = {
+  color: '#99CCCC',
+  failedColor: '#874b4b',
+  thickness: '4px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  location: 'top',
+  inverse: false
+}
+
+Vue.use(VueProgressBar, options)
+
+
 Vue.config.productionTip = false
 
 localStorage.setItem('debug', 'leancloud*')  //开启调试模式
